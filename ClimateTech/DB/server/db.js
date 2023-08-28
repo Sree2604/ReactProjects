5c3e81aa942cca88a95ecf2e8fffe56f5a8b2721
SS-Sarvash <sarvashsaravanan.com>
2023-08-28 15:58:45 +0530

    forked DB

--- ClimateTech/DB/server/db.js
+++ ClimateTech/DB/server/db.js
@@ -1,8 +1,8 @@
 const Pool = require('pg').Pool;
 const pool =new Pool({
     user:"postgres",
-    password:"12345678",
-    database:"postgres",
+    password:"12345",
+    database:"test1",
     port:5432,
     host:"localhost"
 });
